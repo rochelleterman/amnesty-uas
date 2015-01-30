614069713e5fa457805069b26ccc40a3a102d4db
Rochelle Terman <rterman@gmail.com>
2015-01-29 20:40:39 -0800

    finished nyt api search

--- database.R
+++ database.R
@@ -38,6 +38,101 @@ get.variables <- function(variable,row){
   x <- rt[rt$ccode==amnesty$countrycode[row] & rt$year==amnesty$year[row],variable]
   return(x)
 }
+get.variables("polity",200) # testing
 
-get.variables("polity",200)
+# polity
+rows <- 1:nrow(amnesty)
+amnesty$polity <- lapply(rows,get.variables,variable="polity")
+amnesty$polity[amnesty$polity=="integer(0)"] <- NA
+amnesty$polity[grep("NA",amnesty$polity)] <- NA
 
+# polity2
+amnesty$polity2 <- lapply(rows,get.variables,variable="polity2")
+amnesty$polity2[amnesty$polity=="integer(0)"] <- NA
+amnesty$polity2[grep("NA",amnesty$polity)] <- NA
+
+# democ
+amnesty$democ <- lapply(rows,get.variables,variable="democ")
+amnesty$democ[amnesty$democ=="integer(0)"] <- NA
+amnesty$democ[grep("NA",amnesty$democ)] <- NA
+
+# autoc
+
+amnesty$autoc <- lapply(rows,get.variables,variable="autoc")
+amnesty$autoc[amnesty$autoc=="integer(0)"] <- NA
+amnesty$autoc[grep("NA",amnesty$autoc)] <- NA
+
+# physint
+
+amnesty$physint <- lapply(rows,get.variables,variable="physint")
+amnesty$physint[amnesty$physint=="integer(0)"] <- NA
+amnesty$physint[grep("NA",amnesty$physint)] <- NA
+
+# speech
+
+amnesty$speech <- lapply(rows,get.variables,variable="speech")
+amnesty$speech[amnesty$speech=="integer(0)"] <- NA
+amnesty$speech[grep("NA",amnesty$speech)] <- NA
+
+# gdp.pc.wdi
+
+amnesty$gdp.pc.wdi <- lapply(rows,get.variables,variable="gdp.pc.wdi")
+amnesty$gdp.pc.wdi[amnesty$gdp.pc.wdi=="integer(0)"] <- NA
+amnesty$gdp.pc.wdi[grep("NA",amnesty$gdp.pc.wdi)] <- NA
+
+# pop.wdi
+
+amnesty$pop.wdi <- lapply(rows,get.variables,variable="pop.wdi")
+amnesty$pop.wdi[amnesty$pop.wdi=="integer(0)"] <- NA
+amnesty$pop.wdi[grep("NA",amnesty$pop.wdi)] <- NA
+
+# amnesty
+
+amnesty$amnesty <- lapply(rows,get.variables,variable="amnesty")
+amnesty$amnesty[amnesty$amnesty=="integer(0)"] <- NA
+amnesty$amnesty[grep("NA",amnesty$amnesty)] <- NA
+
+# statedept
+
+amnesty$statedept <- lapply(rows,get.variables,variable="statedept")
+amnesty$statedept[amnesty$statedept=="integer(0)"] <- NA
+amnesty$statedept[grep("NA",amnesty$statedept)] <- NA
+
+# milper
+
+amnesty$milper <- lapply(rows,get.variables,variable="milper")
+amnesty$milper[amnesty$milper=="integer(0)"] <- NA
+amnesty$milper[grep("NA",amnesty$milper)] <- NA
+
+# cinc
+
+amnesty$cinc <- lapply(rows,get.variables,variable="cinc")
+amnesty$cinc[amnesty$cinc=="integer(0)"] <- NA
+amnesty$cinc[grep("NA",amnesty$cinc)] <- NA
+
+# domestic9
+
+amnesty$domestic9 <- lapply(rows,get.variables,variable="domestic9")
+amnesty$domestic9[amnesty$domestic9=="integer(0)"] <- NA
+amnesty$domestic9[grep("NA",amnesty$domestic9)] <- NA
+
+# amnesty.uas
+
+amnesty$amnesty.uas <- lapply(rows,get.variables,variable="amnesty.uas")
+amnesty$amnesty.uas[amnesty$amnesty.uas=="integer(0)"] <- NA
+amnesty$amnesty.uas[grep("NA",amnesty$amnesty.uas)] <- NA
+
+# nyt
+
+amnesty$nyt <- lapply(rows,get.variables,variable="nyt")
+amnesty$nyt[amnesty$nyt=="integer(0)"] <- NA
+amnesty$nyt[grep("NA",amnesty$nyt)] <- NA
+
+# region
+
+amnesty$region <- lapply(rows,get.variables,variable="region")
+amnesty$region[amnesty$region=="integer(0)"] <- NA
+amnesty$region[grep("NA",amnesty$region)] <- NA
+
+
+#TODO: Ethiopia countrycode weirdness
